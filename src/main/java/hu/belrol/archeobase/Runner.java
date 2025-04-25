afec828363563de3a5ea917f199a684edd205419
Bellér Roland <bb.roli99@gmail.com>
2025-04-25 08:32:24 +0200

    deployhoz módosítás

---
+++ src/main/java/hu/belrol/archeobase/Runner.java
@@ -0,0 +1,10 @@
+package hu.belrol.archeobase;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class Runner {
+    public static void main(String[] args) throws SQLException, IOException {
+        App.main(args);
+    }
+}
